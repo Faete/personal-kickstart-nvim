5118805d181d6ec40842c1c656db9080bfdfe690
Faete <mattcesena99@gmail.com>
2024-06-02 12:04:17 -0700

    Initial Commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,33 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
+	{
+		"nvim-orgmode/orgmode",
+		event = "VeryLazy",
+		ft = { "org" },
+		config = function()
+			require("orgmode").setup({
+				org_agenda_files = "~/orgfiles/**/*",
+				org_default_notes_file = "~/orgfiles/refile.org",
+			})
+			-- require("nvim-treesitter.configs").setup({
+			-- 	ensure_installed = "all",
+			-- 	ignore_install = { "org" },
+			-- })
+		end,
+	},
+}
