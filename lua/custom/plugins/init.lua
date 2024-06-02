1244fefa04e03d6aea7983a159240acec6b65983
Faete <mattcesena99@gmail.com>
2024-06-02 13:33:25 -0700

    Neorg + Startify

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,6 +15,7 @@ return {
 		},
 		config = true,
 	},
+
 	{
 		"nvim-orgmode/orgmode",
 		event = "VeryLazy",
@@ -28,6 +29,45 @@ return {
 			-- 	ensure_installed = "all",
 			-- 	ignore_install = { "org" },
 			-- })
+		end,
+	},
+
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		version = "*",
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {},
+					["core.concealer"] = {},
+					["core.dirman"] = {
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+							default_workspace = "notes",
+						},
+					},
+				},
+			})
+
+			vim.wo.foldlevel = 99
+			vim.wo.conceallevel = 2
+		end,
+	},
+
+	{
+		"startup-nvim/startup.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("startup").setup()
 		end,
 	},
 }
